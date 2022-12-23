40c32a1fe3eea26c19a5eb2dce8e4ec5bde5acc0
rymiyamoto <ry.miyamoto129@gmail.com>
2022-12-23 11:40:45 +0900

    add

---
+++ service/rakuten_client.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/rymiyamoto/affiliate-api/env"
+)
+
+type (
+	IRakutenClient interface {
+		GetProduct() error
+	}
+
+	RakutenClient struct {
+	}
+)
+
+const (
+	rakutenProductSearchEndpoint = "https://app.rakuten.co.jp/services/api/Product/Search/20170426?format=json"
+)
+
+// NewRakutenClient initialize
+func NewRakutenClient() IRakutenClient {
+	return &RakutenClient{}
+}
+
+// GetProduct 商品取得
+func (s *RakutenClient) GetProduct() error {
+	req, err := http.NewRequest("GET", rakutenProductSearchEndpoint, nil)
+	if err != nil {
+		return fmt.Errorf("failed to make rakuten product search request. err: %w", err)
+	}
+
+	appID, err := env.GetValue("RAKUTEN_APP_ID")
+	if err != nil {
+		return err
+	}
+
+	q := req.URL.Query()
+	q.Add("applicationId", appID)
+	q.Add("productId", "8e27892c206a7118a74445c5a0825a41")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to request rakuten product search. err: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to perse response rakuten product search. err: %w", err)
+	}
+
+	fmt.Println(string(body))
+
+	return nil
+}
--- usecase/rakuten.go
+++ usecase/rakuten.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"github.com/rymiyamoto/affiliate-api/flag"
 	"github.com/rymiyamoto/affiliate-api/log"
+	"github.com/rymiyamoto/affiliate-api/service"
 )
 
 type (
@@ -11,16 +12,22 @@ type (
 	}
 
 	Rakuten struct {
+		rakutenClientService service.IRakutenClient
 	}
 )
 
 // NewRakuten initialize
 func NewRakuten() IRakuten {
-	return &Rakuten{}
+	return &Rakuten{
+		rakutenClientService: service.NewRakutenClient(),
+	}
 }
 
 // Exec 実行
 func (u *Rakuten) Exec(f *flag.Affiliate) error {
 	log.Info("rakuten")
+	if err := u.rakutenClientService.GetProduct(); err != nil {
+		return err
+	}
 	return nil
 }
